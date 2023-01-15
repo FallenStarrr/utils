31623ec8564297a73706d956a6ee8e1cba419343
Vlad <juzopigeon@gmail.com>
2023-01-15 12:59:12 +0600

    Create utils.go

---
+++ utils.go
@@ -0,0 +1,39 @@
+package utils
+
+import "net/http"
+import "io/ioutil"
+import "fmt"
+
+
+
+
+
+type Error struct {
+	Message string
+}
+
+func DecodeRequestBody(req *http.Request) []byte {
+				
+		body, err   := ioutil.ReadAll(req.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		return body
+}
+
+func DecodeResponseBody(res *http.Response) []byte {
+				
+	body, err   := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return body
+}
+
+
+func CheckError(err error) {
+		
+  if err != nil {
+		fmt.Println(err)
+	}
+}
