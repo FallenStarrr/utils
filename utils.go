8362c70ed7318dce4304d677bd2582b1355874f7
Vlad <juzopigeon@gmail.com>
2023-01-16 16:09:04 +0600

    Update utils.go

--- utils.go
+++ utils.go
@@ -3,7 +3,7 @@ package utils
 import "net/http"
 import "io/ioutil"
 import "fmt"
-
+import "strings"
 
 
 
@@ -28,6 +28,36 @@ func DecodeResponseBody(res *http.Response) []byte {
 		fmt.Println(err)
 	}
 	return body
+}
+
+func checkBearerToken(header interface{}) bool {
+	
+	var authHead string
+	auth := map[string]interface{}
+	
+	switch(header.(type)) {
+		case http.Request:
+		auth["request"] = header.(http.Request).Header.Get("Authorization")
+		case *http.Response:
+		auth["response"] = header.(*http.Response).Header.Get("Authorization")
+	}
+	if auth["request"].(string) != "" {
+		authHead = auth["request"].(string)
+	} else if auth["response"].(string) != "" {
+		authHead = auth["response"].(string)
+	}
+	
+	
+	tokenType := strings.Split(authHead, " ")[0]
+	token := strings.Split(authHead, " ")[1]
+	
+	if tokenType != "Bearer" {
+		return false
+	}
+	if token == "" {
+		return false
+	}
+	return true
 }
 
 
